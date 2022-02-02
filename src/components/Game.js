2456bdb2c1bca538687d4ad9c9a87fff6c744a38
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-01 19:45:24 -0500

    add game logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,15 +10,53 @@ export default class Game extends Component {
 			history: [{ squares: Array(9).fill(null) }],
 		};
 	}
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		const winner = calculateWinner(squares);
+		if (winner || squares[i]) {
+			return;
+		}
+		squares[i] = this.state.xIsNext ? "x" : "0";
+		this.setState({
+			history: history.concat({ squares: squares }),
+			xIsNext: !this.state.xIsNext,
+			stepNumber: history.length,
+		});
+	}
 	render() {
 		const history = this.state.history;
 		const current = history[this.state.stepNumber];
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board onClick={(i) => this.onClick(i)} squares={current.squares} />
+					<Board
+						onClick={(i) => this.handleClick(i)}
+						squares={current.squares}
+					/>
 				</div>
 			</div>
 		);
 	}
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length++; ) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
